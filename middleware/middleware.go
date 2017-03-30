256c81799b1199c1327576d1de33ee354209a753
Erich Nascimento <erichnascimento@gmail.com>
2017-03-29 22:09:11 -0300

    Implements new concept using handlefunc

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,26 +2,11 @@ package middleware
 
 import (
 	"github.com/erichnascimento/rocket"
+	"net/http"
 )
 
 type HandleFunc func(ctx *rocket.Context)
 
 type Middleware interface {
-	CreateHandle(next HandleFunc) HandleFunc
-}
-
-type DynaHandleFunc func(ctx *rocket.Context, next HandleFunc)
-
-type dynamiddleware struct {
-	handler DynaHandleFunc
-}
-
-func NewDynamiddleware(handler DynaHandleFunc) Middleware {
-	return &dynamiddleware{handler}
-}
-
-func (d *dynamiddleware) CreateHandle(next HandleFunc) HandleFunc {
-	return func(ctx *rocket.Context) {
-		d.handler(ctx, next)
-	}
+	Mount(next http.HandlerFunc) http.HandlerFunc
 }
