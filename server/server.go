47ab5bf5942668b7cfb1382d2e4b8d8d0befaab1
Erich Nascimento <erichnascimento@gmail.com>
2018-08-02 22:27:51 -0300

    Rename rocket to server

--- server/server.go
+++ server/server.go
@@ -4,16 +4,15 @@ import (
 	"net/http"
 
 	"github.com/erichnascimento/rocket/middleware"
-	"github.com/erichnascimento/rocket/server/response"
 )
 
-type Rocket struct {
+type Server struct {
 	server      *http.Server
 	handler     http.HandlerFunc
 	middlewares []middleware.Middleware
 }
 
-func (s *Rocket) ListenAndServe(addr string) error {
+func (s *Server) ListenAndServe(addr string) error {
 	s.handler = s.finalHandlerFunc
 	for i := len(s.middlewares) - 1; i >= 0; i-- {
 		s.handler = s.middlewares[i].Mount(s.handler)
@@ -27,22 +26,21 @@ func (s *Rocket) ListenAndServe(addr string) error {
 	return s.server.ListenAndServe()
 }
 
-func (s *Rocket) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
-	wrw := response.WrapResponseWriter(rw)
-	s.handler(wrw, rq)
+func (s *Server) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
+	s.handler(rw, rq)
 }
 
-func (s *Rocket) Use(m middleware.Middleware) {
+func (s *Server) Use(m middleware.Middleware) {
 	if s.middlewares == nil {
 		s.middlewares = make([]middleware.Middleware, 0)
 	}
 	s.middlewares = append(s.middlewares, m)
 }
 
-func (s *Rocket) finalHandlerFunc(rw http.ResponseWriter, rq *http.Request) {
+func (s *Server) finalHandlerFunc(rw http.ResponseWriter, rq *http.Request) {
 	//log.Println("Done ", rq.Context())
 }
 
-func NewRocket() *Rocket {
-	return new(Rocket)
+func NewServer() *Server {
+	return new(Server)
 }
